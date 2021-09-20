36fca04237924f05a94f1a183bb7bd26f6dd293d
LeonardoESousa <leonardo.sousa137@gmail.com>
2021-09-20 11:23:38 +0200

    Fixed andamento function to handle emission with ss-pcm

--- leox.py
+++ leox.py
@@ -82,11 +82,7 @@ def pega_geom(freqlog):
     try:
         G = G[1:,:]                 
     except:
-<<<<<<< HEAD:leox.py
         print("No geometry in the log file! Goodbye!")
-=======
-        print("Sem geometria no log de frequência! Adeus!")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
         sys.exit()
     return G, atomos
 ###############################################################
@@ -100,35 +96,7 @@ def salva_geom(G,atomos):
     print("The optimized geometry that is used is saved in the opt_geom.txt file!")
 ###############################################################
 
-<<<<<<< HEAD:leox.py
-=======
-def salva_geom(G,atomos):
-    atomos = np.array([atomos]).astype(float)
-    atomos = atomos.T
-    G = np.hstack((atomos,G))
-    np.savetxt('opt_geom.txt', G, delimiter='\t', fmt=['%1.1u','%+1.5f','%+1.5f','%+1.5f'])
-    print("A geometria otimizada que vai ser usada está salva no arquivo opt_geom.txt!")
-
-
-def pega_massas(freqlog,G):
-    _ , M = pega_freq(freqlog)
-    num_atom = np.shape(G)[0]
-    massas = []
-    with open(freqlog, 'r') as f:
-        for line in f:
-            if "has atomic number" in line:
-                line = line.split()
-                massas.append(float(line[-1]))
-                massas.append(float(line[-1]))
-                massas.append(float(line[-1]))
-    massas = np.expand_dims(np.asarray(massas),axis=1)
-    atomos = np.zeros((3*num_atom,1))
-    for _ in range(0,len(M)):
-        atomos = np.hstack((atomos,massas))
-    massas = atomos[:,1:]
-    return massas*amu
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
-
+##GETS NORMAL COORDINATES IN HIGH PRECISION####################
 def pega_modosHP(G, freqlog):
     F, M = pega_freq(freqlog)
     n = -1
@@ -157,10 +125,10 @@ def pega_modosHP(G, freqlog):
     for _ in range(0,3*num_atom):
         MM = np.vstack((MM,M))
     M = MM[1:,:]
-    #NC = NC*np.sqrt(massas/M)
-    #print(np.round(np.matmul(NC.T,NC),2))
     return NC
+###############################################################
 
+##GETS NORMAL COORDINATES IN REGULAR PRECISION#################
 def pega_modosLP(G,freqlog):
     F, M = pega_freq(freqlog)
     C = []
@@ -207,9 +175,10 @@ def pega_modosLP(G,freqlog):
     for i in range(0,3*num_atom):
         MM = np.vstack((MM,M))
     M = MM[1:,:]
-    #D = D*np.sqrt(massas/M)
     return D
+###############################################################
 
+##DETECTS WHETHER HIGH PRECISION IS USED#######################
 def pega_modos(G,freqlog):
     x = 'LP'
     with open(freqlog, 'r') as f:
@@ -221,7 +190,9 @@ def pega_modos(G,freqlog):
         return pega_modosLP(G,freqlog)
     else:
         return pega_modosLP(G,freqlog)
+###############################################################
 
+##DISPLACE GEOMETRY IN DIRECTIONS WITH IMAGINARY FREQ##########
 def shake(freqlog, T):
     F, M = pega_freq(freqlog)
     G, atomos = pega_geom(freqlog)
@@ -231,11 +202,7 @@ def shake(freqlog, T):
     A2 = np.zeros((3*num_atom,1))
     F = F[F < 0]
     if len(F) == 0:
-<<<<<<< HEAD:leox.py
         print("No imaginary frquencies in the log file. Goodbye!")
-=======
-        print("Não há frequências imaginárias! Adeus!")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
         sys.exit()
     F = -1*F
     for i in range(len(F)): # LL:
@@ -247,7 +214,6 @@ def shake(freqlog, T):
     Gfinal  = A1 + G
     Gfinal2 = A2 + G
     with open("shaken.xyz", 'w') as f:
-<<<<<<< HEAD:leox.py
         f.write('#Geometry with displacement of '+str(T)+" A:\n" )
         for k in range(0, np.shape(Gfinal)[0]):
             text = "%2s % 2.14f % 2.14f % 2.14f" % (atomos[k],Gfinal[k,0],Gfinal[k,1],Gfinal[k,2])
@@ -257,44 +223,24 @@ def shake(freqlog, T):
             text = "%2s % 2.14f % 2.14f % 2.14f" % (atomos[k],Gfinal2[k,0],Gfinal2[k,1],Gfinal2[k,2])
             f.write(text+"\n")
     print("There are 2 geometries saved on shaken.xyz!")
-=======
-        f.write('#Geometria com deformação de '+str(T)+" A:\n" )
-        for k in range(0, np.shape(Gfinal)[0]):
-            text = "%2s % 2.14f % 2.14f % 2.14f" % (atomos[k],Gfinal[k,0],Gfinal[k,1],Gfinal[k,2])
-            f.write(text+"\n")
-        f.write('\n#Geometria com deformação de '+str(-T)+" A:\n" )
-        for k in range(0, np.shape(Gfinal2)[0]):
-            text = "%2s % 2.14f % 2.14f % 2.14f" % (atomos[k],Gfinal2[k,0],Gfinal2[k,1],Gfinal2[k,2])
-            f.write(text+"\n")
-    print("Há 2 geometrias salvas no arquivo shaken.xyz!")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
+###############################################################
 
-
+##SAMPLES GEOMETRIES###########################################
 def sample_geom(freqlog, num_geoms, T, header, bottom):
     F, M = pega_freq(freqlog)
     if F[0] < 0:
-<<<<<<< HEAD:leox.py
         print("Imaginary frequency! Goodbye!")
         sys.exit()
     try:
         os.mkdir('Geometries')
     except:
         pass        
-=======
-        print("Frequência negativa! Volte para casa 1")
-        sys.exit()
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     G, atomos = pega_geom(freqlog)
     salva_geom(G,atomos)
     NNC = pega_modos(G,freqlog)
     num_atom = np.shape(G)[0]   
-<<<<<<< HEAD:leox.py
     print("\nGenerating geometries...\n")
     with open('Magnitudes.lx', 'w') as file:
-=======
-    print("\nGerando geometrias...\n")
-    with open('Magnitudes.txt', 'w') as file:
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
         for n in range(1,num_geoms+1):
             A = np.zeros((3*num_atom,1))
             numbers = []
@@ -309,11 +255,7 @@ def sample_geom(freqlog, num_geoms, T, header, bottom):
             np.savetxt(file, numbers, delimiter='\t', fmt='%s')
             A = np.reshape(A,(num_atom,3))
             Gfinal = A + G  
-<<<<<<< HEAD:leox.py
             with open("Geometries/Geometry-"+str(n)+"-.com", 'w') as f:
-=======
-            with open("Geometria-"+str(n)+"-.com", 'w') as f:
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
                     f.write(header.replace("UUUUU",str(n)))
                     for k in range(0, np.shape(Gfinal)[0]):
                         text = "%2s % 2.14f % 2.14f % 2.14f" % (atomos[k],Gfinal[k,0],Gfinal[k,1],Gfinal[k,2])
@@ -321,58 +263,21 @@ def sample_geom(freqlog, num_geoms, T, header, bottom):
                     f.write("\n"+bottom.replace("UUUUU",str(n)))
             progress = 100*n/num_geoms
             text = "%2.1f" % progress
-<<<<<<< HEAD:leox.py
             print(' ', text, "% of the geometries done.",end="\r", flush=True)
-=======
-            print(text, "% das geometrias feitas.",end="\r", flush=True)
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     
-    print("\n\nC'est fini! Ready to run.")   
-    
-    #Plots the histograms of normal coordinates
-    #random.seed(10)
-    #ind =0
-    #for T in [0, 300]:
-    #   for i in range(0,len(F)):
-    #       x = np.linspace(-5, 5, 10000) #ja em angstrom
-    #       boltz = np.tanh(hbar*F[i]/(2*kb*T))
-    #       print(hbar*F[i])
-    #       #print(hbar, F[i]/(2*np.pi*(10**12)), boltz, np.sinh(2*boltz), np.tanh(boltz))
-    #       #prob1 = np.sqrt((M[i]*F[i])/(pi*hbar2))*np.exp(-M[i]*F[i]*((x*(10**(-10)))**2)/hbar2)*(abs(x[1]-x[0])*10**(-10)) #sem temperatura
-    #       #prob2 = (((2*np.pi*kb*T*e)/(M[i]*(F[i]**2)))**(-1/2))*np.exp(-M[i]*(F[i]**2)*((x*(10**(-10)))**2)/(2*kb*T*e))*(abs(x[1]-x[0])*10**(-10))#    np.sqrt((M[i]*F[i])/(2*np.pi*hbar2*np.sinh(2*boltz)))*np.exp(-M[i]*F[i]*((x*(10**(-10)))**2)*np.tanh(boltz)/hbar2)*(abs(x[1]-x[0])*10**(-10))
-    #       prob2 = np.sqrt((M[i]*F[i]*(boltz))/(np.pi*hbar2))*np.exp(-M[i]*F[i]*((x*(10**(-10)))**2)*(boltz)/hbar2)*(abs(x[1]-x[0])*10**(-10))
-    #       #q = random.choices(x, prob1, k=1000)
-    #       q2 = random.choices(x, prob2, k=1000)
-    #       #plt.hist(q, bins=40)
-    #       plt.subplot(2,3,ind+1)
-    #       ind += 1
-    #       plt.xlim([-0.3,0.3])
-    #       plt.ylim([0,80])
-    #       #plt.plot(x,prob2)
-    #       plt.hist(q2, bins=40)
-    #plt.show()
+    print("\n\nDone! Ready to run.")   
+###############################################################    
+
             
 
-<<<<<<< HEAD:leox.py
 def gather_data(opc, tipo):
     files = [file for file in os.listdir('Geometries') if ".log" in file and "Geometry-" in file ]    
     files = sorted(files, key=lambda file: float(file.split("-")[1])) 
-=======
-def gather_data(G, freqlog, opc, tipo):
-    F, M = pega_freq(freqlog)
-    NNC = pega_modos(G,freqlog)
-    massas = pega_massas(freqlog,G)
-    files = [file for file in os.listdir('.') if ".log" in file and "Geometria-" in file ]    
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     with open("Samples.lx", 'w') as f:
         for file in files:
             num = file.split("-")[1]
             broadening = opc
-<<<<<<< HEAD:leox.py
             f.write("Geometry "+num+":  Vertical transition (eV) Oscillator strength Vibronic Shift (eV) Broadening Factor (eV) \n")
-=======
-            f.write("Geometria "+num+":  Vertical transition (eV) Oscillator strength Vibronic Shift (eV) Broadening Factor (eV) \n")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
             numeros, energies, fs, scfs = [], [], [], []
             corrected, total_corrected = -1, -1
             with open(file, 'r') as g:
@@ -398,17 +303,20 @@ def gather_data(G, freqlog, opc, tipo):
                     energy = str(np.round(total_corrected - scfs[-1],3))
                     vibronic = 0
                     f.write("Excited State 1:\t"+energy+"\t"+fs[0]+"\t"+str(vibronic)+"\t"+str(broadening)+"\n")
+                elif corrected == -1 and tipo == 'emi':
+                    vibronic = 0
+                    f.write("Excited State "+numeros[0]+"\t"+energies[0]+"\t"+fs[0]+"\t"+str(vibronic)+"\t"+str(broadening)+"\n")
                 else:
                     for i in range(len(energies)):
                         vibronic = 0
                         f.write("Excited State "+numeros[i]+"\t"+energies[i]+"\t"+fs[i]+"\t"+str(vibronic)+"\t"+str(broadening)+"\n")
                 f.write("\n")   
 
-
+##NORMALIZED GAUSSIAN##########################################
 def gauss(x,v,s):
     y =  (1/(np.sqrt(2*np.pi)*s))*np.exp(-0.5*((x-v)/s)**2)
     return y
-
+###############################################################
 
 def spectra(tipo, num_ex, nr):
     if tipo == "abs":
@@ -514,14 +422,19 @@ def batch(gauss):
 def andamento():
     coms = [file for file in os.listdir("Geometries") if 'Geometry-' in file and '.com' in file]
     logs = [file for file in os.listdir("Geometries") if 'Geometry-' in file and '.log' in file]
+    factor = 1
+    with open('Geomtries/'+coms[0], 'r') as f:
+        for line in f:
+            if 'Link1' in line:
+                factor = 2
     count = 0
     for file in logs:
-        with open(file, 'r') as f:
+        with open('Geometries/'+file, 'r') as f:
             for line in f:
                 if "Normal termination" in line:
                     count += 1
     print("\n\nThere are", count, "completed calculations out of", len(coms), "inputs")                
-    print("It is", np.round(100*count/len(coms),1), "% done.")                
+    print("It is", np.round(100*count/(factor*len(coms)),1), "% done.")                
 
 def busca_log(frase):                   
     files = [file for file in os.listdir('.') if ".log" in file and "Geometry-" not in file]
@@ -549,7 +462,6 @@ print("#        #       #    #   # #  ")
 print("#        #       #    #  #   # ")
 print("#######  ######   ####  #     #")
 print("----SPECTRA FOR THE PEOPLE!----\n")
-<<<<<<< HEAD:leox.py
 print("Your options:\n")
 print("I have frequency calculations done. I want to generate the inputs for the spectrum calculation - type 1")
 print("My inputs are set, I want to run the spectrum calculations - type 2")
@@ -559,27 +471,12 @@ print("I want to shake a molecule to help me get rid of imaginary frequencies - 
 op = input()
 if op == '1':
     freqlog = busca_log("Is this the log file for the frequency calculation?")
-=======
-print("Quer fazer o que??\n")
-print("Tenho só o log de frequência. Quero gerar geometrias - digite 1")
-print("Geometrias prontas, quero botar para rodar com o ts - digite 2")
-print("Tudo pronto, quero gerar o espectro - digite 3")
-print("Quero saber a quantas anda essa joça - digite 4")
-print("Quero sacudir uma molécula para me livrar de frequências imaginárias - digite 5")
-op = input()
-if op == '1':
-    freqlog = busca_log("É esse o log de frequência?")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     base, temtd, nproc, mem = busca_input(freqlog)
     print("\n"+base)
     resp = input("Are basis and functional correct? If so, pres Enter. Otherwise, type functional/basis.\n")
     if resp != "":
         base = resp 
-<<<<<<< HEAD:leox.py
     adicional = input("If there are extra keywords, type them. Otherwise, press Enter.\n")
-=======
-    adicional = input("Se houver comandos adicionais, digite-os. Caso contrário, aperte Enter.\n")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     base += " "+adicional
     num_ex = input("How many excited states?\n")
     try:
@@ -589,7 +486,6 @@ if op == '1':
         sys.exit()
     print('%nproc='+nproc)    
     print('%mem='+mem)
-<<<<<<< HEAD:leox.py
     procmem = input('Are Nproc and Mem correct? y or n?\n')
     if procmem.lower() != 'y':
         nproc = input('nproc?\n')
@@ -605,23 +501,6 @@ if op == '1':
         if solv.lower() == "read":
             eps1 = input("Type the static dielectric constant.\n")
             eps2 = input("Type the dynamic dielectric constant (n^2).\n")
-=======
-    procmem = input('Nproc e Mem estão corretos? s ou n?\n  ')
-    if procmem != 's':
-        nproc = input('nproc?\n')
-        mem = input("mem?\n")
-    num_geoms = int(input("Quantas geometrias?\n")) #numero de gometrias para gerar
-    tda = 'TD'
-    tamm = input('Usar TDA (Tamm-Dancoff Approximation)? s ou n?\n')
-    if tamm == 's':
-        tda = 'TDA'
-    pcm = input("Incluir solvente (SS-PCM)? s ou n?\n")
-    if pcm == 's':
-        solv = input("Qual o solvente? Se quiser especificar as constantes dielétricas, digite read.\n")
-        if solv == "read":
-            eps1 = input("Digite a constante dielétrica estática.\n")
-            eps2 = input("Digite a constante dielétrica dinâmica (n^2).\n")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
             try:
                 float(eps1)
                 float(eps2)
@@ -633,21 +512,12 @@ if op == '1':
             solv = "SOLVENT="+solv
             epss = "\n"
         if temtd:
-<<<<<<< HEAD:leox.py
             print("Inputs suitable for emission spectra!\n")
             header = "%chk=step_UUUUU.chk\n%nproc="+nproc+"\n%mem="+mem+"\n# "+base+" "+tda+"=(NSTATES=3) SCRF=(CorrectedLR,NonEquilibrium=Save,"+solv+")\n\nTITLE\n\n0 1\n"
             bottom = epss+"\n--Link1--\n%nproc="+nproc+"\n%mem="+mem+"\n%oldchk=step_UUUUU.chk\n%chk=step2_UUUUU.chk\n# "+base+" GUESS=READ GEOM=CHECKPOINT SCRF(NonEquilibrium=Read,"+solv+")\n\nTITLE\n\n0 1\n\n"+epss
         else:
             print("Inputs suitable for absortion spectra!!\n")
             header = "%nproc="+nproc+"\n%mem="+mem+"\n# "+base+" SCRF=(CorrectedLR,"+solv+") "+tda+"=(NSTATES=3)\n\nTITLE\n\n0 1\n"
-=======
-            print("Preparando inputs para espectro de emissão!\n")
-            header = "%chk=stepUUUUU.chk\n%nproc="+nproc+"\n%mem="+mem+"\n# "+base+" "+tda+"=(NSTATES=1,EQSOLV) SCRF=(CorrectedLR,"+solv+") IOp(10/74=20)\n\nTITLE\n\n0 1\n"
-            bottom = "NonEq=Write\n\n--Link1--\n%oldchk=stepUUUUU.chk\n%chk=step2UUUUU.chk\n# "+base+" GUESS=READ GEOM=CHECKPOINT SCRF("+solv+")\n\nTITLE\n\n0 1\n\nNONEQ=Read\n"+epss
-        else:
-            print("Preparando inputs para espectro de absorção!\n")
-            header = "%nproc="+nproc+"\n%mem="+mem+"\n# "+base+" SCRF=(CorrectedLR,"+solv+") "+tda+"=(NSTATES=1,NonEqSolv) IOP(10/74=10)\n\nTITLE\n\n0 1\n"
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
             bottom = epss
     elif pcm == 'n':
         header = "%nproc="+nproc+"\n%Mem="+mem+"\n# "+tda+"=(NStates="+str(num_ex)+") "+base+" \n\nTITLE\n\n0 1\n"
@@ -655,15 +525,9 @@ if op == '1':
     else:
         print("It should be y or n. Goodbye!.")
         sys.exit()
-<<<<<<< HEAD:leox.py
     T = float(input("Temperature in Kelvin?\n")) #K
     if T <= 0:
         print("Have you heard about absolute zero? Goodbye!")
-=======
-    T = float(input("Temperatura em Kelvin?\n")) #K
-    if T <= 0:
-        print("Você está em violação da Terceira Lei da Termodinâmica... Aí não dá.")
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
         sys.exit()
     sample_geom(freqlog, num_geoms, T, header, bottom)    
 elif op == '3':
@@ -692,11 +556,7 @@ elif op == '3':
         sys.exit()
     num_ex = range(0,estados+1)
     num_ex = list(map(int,num_ex))
-<<<<<<< HEAD:leox.py
     gather_data(opc, tipo)
-=======
-    gather_data(G,freqlog, opc, tipo)
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
     spectra(tipo, num_ex, nr)
 elif op == '2':
     op = input("O ts está pronto já? s ou n?\n")
@@ -708,7 +568,6 @@ elif op == '2':
 elif op == '4':
     andamento()
 elif op == '5':
-<<<<<<< HEAD:leox.py
     freqlog = busca_log("Is this the frequency calculation log file?")
     T = float(input("Magnitude of the displacement in Å? \n")) #K
     shake(freqlog,T)
@@ -720,11 +579,3 @@ else:
     
         
 
-=======
-    freqlog = busca_log("É esse o log de frequência?")
-    T = float(input("Magnitude da deformação? (Algo entre 0.1 e 0.5 Angstrom)\n")) #K
-    shake(freqlog,T)
-else:
-    print("Tem que ser um dos cinco, animal!")
-    sys.exit()
->>>>>>> f6e4fc07a97286085a4954e159b92cb477ea906c:leox2.py
