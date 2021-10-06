8fc7e35e3c8ce9fd8c953fcde73c21c95fe934af
LeonardoESousa <leonardo.sousa137@gmail.com>
2021-10-06 21:13:27 +0200

    Updated examples

---
+++ batch_examples/batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mem=50GB
+#SBATCH --time=1-0
+#SBATCH -N 1
+#SBATCH -n 40
+#SBATCH --partition=xeon40
+
+module load Gaussian/16
+export GAUSS_SCRDIR=/scratch/ledso/
+
+
+g16 $1
--- batch_examples/slurm.sh
+++ batch_examples/slurm.sh
@@ -1,5 +1,5 @@
 #The script must include the specific command for job submission used in you batch system followed by $1.
-#In this case here, the batch.sh is another script that contains the details of submission and takes as 
+#In this case here, batch.sh is another script that contains the details of submission and takes as 
 #argument the input file
 
 sbatch batch.sh $1
