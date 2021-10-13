853113003b0dee938b9d062d17e0d1b46a68271d
LeonardoESousa <leonardo.sousa137@gmail.com>
2021-10-13 11:31:05 +0200

    Added details

--- batch_examples/ts.sh
+++ batch_examples/ts.sh
@@ -1,3 +1,4 @@
-#The script must include the specific command for job submission used in you batch system followed by $1. 
+#The script must include the specific command for job submission used in you batch system followed by $1.
+#In the case of ts this is just ts plus the appropriate gaussian command followed by $1 
 
 ts g16 $1
