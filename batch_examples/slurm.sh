280a5c59fe0be34fc4417cd54e005c10b0cc4a74
LeonardoESousa <leonardo.sousa137@gmail.com>
2021-10-13 12:37:48 +0200

    Updated example

--- batch_examples/slurm.sh
+++ batch_examples/slurm.sh
@@ -1,5 +1,5 @@
 #The script must include the specific command for job submission used in you batch system followed by $1.
 #In this case here, batch.sh is another script that contains the details of submission and takes as 
-#argument the input file
+#argument the input file. Both slurm.sh and batch.sh files must be present before running option 2.
 
 sbatch batch.sh $1
