4057abeaaa9efb77d306787b760c3fa3e18ad9bb
LeonardoESousa <leonardo.sousa137@gmail.com>
2024-11-18 15:33:49 +0100

    new omega tuning algorithm

--- lx/omega.py
+++ lx/omega.py
@@ -143,7 +143,7 @@ def rodar_omega(atomos, geom, base, nproc, mem, omega, op, batch_file, gaussian,
     J = np.sqrt(
         ((homo_neutro + cation - neutro) ** 2 + (homo_anion + neutro - anion) ** 2)
     ) * (27.2114)
-    return J, geom, atomos
+    return J
 
 
 ###############################################################
@@ -161,10 +161,54 @@ def write_tolog(omegas, Js, frase):
 
 ###############################################################
 
+def fetch_grad(x, f, i):
+    """
+    Estimate the gradient at x[i] using finite differences and Lagrange interpolation.
+
+    Parameters:
+    x (list or array): x values.
+    f (list or array): Corresponding f(x) values.
+    i (int): Index of the point to estimate the gradient.
+
+    Returns:
+    float: Estimated gradient at x[i].
+    """
+    if len(x) == 1:
+        # Only one point; gradient is undefined, return 0
+        return 0.0
+
+    if len(x) == 2:
+        # Two points; compute simple finite difference
+        return (f[1] - f[0]) / (x[1] - x[0])
+
+    if i == 0:
+        # Forward finite difference for the first point
+        return (f[1] - f[0]) / (x[1] - x[0])
+
+    if i == len(x) - 1:
+        # Backward finite difference for the last point
+        return (f[-1] - f[-2]) / (x[-1] - x[-2])
+
+    # General case: Use Lagrange interpolation for three points
+    x0, x1, x2 = x[i - 1], x[i], x[i + 1]
+    f0, f1, f2 = f[i - 1], f[i], f[i + 1]
+
+    # Compute denominators for the Lagrange basis polynomials
+    denom0 = (x0 - x1) * (x0 - x2)
+    denom1 = (x1 - x0) * (x1 - x2)
+    denom2 = (x2 - x0) * (x2 - x1)
+
+    # First derivative (gradient) using corrected Lagrange formula
+    l0p = (x1 - x2) / denom0
+    l1p = (2 * x1 - x0 - x2) / denom1
+    l2p = (x1 - x0) / denom2
+    grad = f0 * l0p + f1 * l1p + f2 * l2p
+
+    return grad
 
 def main():
     geomlog = sys.argv[1]
-    base = sys.argv[2]
+    basis = sys.argv[2]
     nproc = sys.argv[3]
     mem = sys.argv[4]
     omega1 = sys.argv[5]
@@ -190,51 +234,74 @@ def main():
     else:
         numjobs = 1
     omegas, Js = [], []
-    oms, jotas = [], []
     try:
-        with open("omega.lx", "r",encoding='utf-8') as f:
-            for line in f:
-                line = line.split()
-                if len(line) == 2 and "#" not in line:
-                    om = float(line[0])
-                    omegas.append(om)
-                    Js.append(float(line[1]))
-        menor = omegas[Js.index(min(Js))]
-        G, atomos = lx.parser.pega_geom(f"Logs/OPT_{menor:05.0f}_.log")
+        data = np.loadtxt("omega.lx", dtype=float)
+        if data.ndim == 1:
+            data = data.reshape(1, -1)
+        omegas = data[:, 0].tolist()
+        Js = data[:, 1].tolist()
     except FileNotFoundError:
-        G, atomos = lx.parser.pega_geom(geomlog)
+        pass    
 
-    while passo > 25:
+    iteration = 0
+    while iteration < 100:
         if omega1 in omegas:
             ind = omegas.index(omega1)
             J = Js[ind]
         else:
-            J, G, atomos = rodar_omega(
-                atomos, G, base, nproc, mem, omega1, op, script, gaussian, numjobs
-            )
+            try:
+                #find existing omega closest to omega1
+                d_omega = [abs(omega1 - om) for om in omegas]
+                geom_log = omegas[d_omega.index(min(d_omega))]
+                geom_log = f"Logs/OPT-{geom_log:3.0f}-.log"
+                G, atomos = lx.parser.pega_geom(geom_log)
+            except (ValueError, FileNotFoundError):
+                G, atomos = lx.parser.pega_geom(geomlog)   
+            J = rodar_omega(
+                atomos, G, basis, nproc, mem, omega1, op, script, gaussian, numjobs)
             omegas.append(omega1)
             Js.append(J)
-        oms.append(omega1)
-        jotas.append(J)
-        try:
-            if jotas[-1] - jotas[-2] > 0:
-                passo = int(passo / 2)
-                sign = -1 * np.sign(int(oms[-1]) - int(oms[-2]))
-            else:
-                sign = +1 * np.sign(int(oms[-1]) - int(oms[-2]))
-            omega1 += sign * passo
+        omegas, Js = map(list, zip(*sorted(zip(omegas, Js))))
+        #index of min J
+        ind = Js.index(min(Js))
+        omega1 = omegas[ind]
+        grad = fetch_grad(omegas, Js, ind)
+        
+        if grad == 0:
+            delta_omega = passo
+        else:
+            delta_omega = -1*Js[ind] / grad
+        
+        if ind == len(Js) - 1:
+            max_omega = 5000
+        else:
+            max_omega = omegas[ind+1]
+        if ind == 0:
+            min_omega = 0
+        else:
+            min_omega = omegas[ind-1]
+        
+        omega1 += delta_omega
+        omega1 = np.round(omega1, 0)
+        if min_omega > omega1 or omega1 > max_omega or int(omega1) in omegas:
+            left = (min_omega - omegas[ind])/2
+            right = (max_omega - omegas[ind])/2
+            omega1 =  omegas[ind] + max(left,right,key=abs)
+        omega1 = int(omega1)
+        if max_omega - min_omega <= 20:
+            break
 
-        except IndexError:
-            omega1 += passo
-        write_tolog(omegas, Js, "#Best value so far:")
+
+        write_tolog(omegas, Js, f"#Best value so far:")
+        iteration += 1
 
     write_tolog(omegas, Js, "#Done! Optimized value:")
     menor = omegas[Js.index(min(Js))]
     log = f"Logs/OPT_{menor:05.0f}_.log"
     G, atomos = lx.parser.pega_geom(log)
-    base, _, nproc, mem, scrf, _ = lx.parser.busca_input(log)
+    basis, _, nproc, mem, scrf, _ = lx.parser.busca_input(log)
     cm = lx.parser.get_cm(log)
-    header = f"%nproc={nproc}\n%mem={mem}\n# {base} {scrf}\n\nTITLE\n\n{cm}\n"
+    header = f"%nproc={nproc}\n%mem={mem}\n# {basis} {scrf}\n\nTITLE\n\n{cm}\n"
     lx.tools.write_input(atomos, G, header, "", "tuned_w.com")
 
 
